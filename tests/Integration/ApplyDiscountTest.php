02411ce70368751c246a02406bf16e5b868b781c
Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>
2022-05-14 11:00:20 +0430

    Refactor discount action test

--- tests/Integration/ApplyDiscountTest.php
+++ tests/Integration/ApplyDiscountTest.php
@@ -10,7 +10,7 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class DiscountServiceTest extends TestCase
+class ApplyDiscountTest extends TestCase
 {
     use RefreshDatabase;
 
